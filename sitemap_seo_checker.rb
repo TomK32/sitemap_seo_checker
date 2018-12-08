75edb69391728225f7b8dd60f81e49e3ec70c725
Thomas R. Koll <tomk32@tomk32.de>
2018-12-08 17:35:27 +0100

    Process urls from the sitemap and write the data into an excel sheet

--- sitemap_seo_checker.rb
+++ sitemap_seo_checker.rb
@@ -5,8 +5,14 @@ require 'bundler'
 
 Bundler.require
 require 'optparse'
+require 'open-uri'
+require 'axlsx'
 
-options = {sitemap: 'sitemap.xml'}
+options = {sitemap: 'sitemap.xml', domain: ARGV[0], excel_file: 'sitemap-check-' + ARGV[0] + '.xls'}
+if ! options[:domain].match(/^http/)
+  options[:domain] = 'https://' + options[:domain]
+end
+
 OptionParser.new do |opts|
   opts.banner = "Usage: #{File.basename(__FILE__)} [options]"
 
@@ -16,15 +22,33 @@ OptionParser.new do |opts|
   opts.on('-s', '--sitemap', "Sitemap.xml path") do |s|
     options[:sitemap] = s
   end
+  opts.on('-e', '--excel', 'Excel file to output') do |e|
+    options[:excel_file] = e
+  end
 end.parse!
 
-options[:domain] = ARGV[0]
-if ! options[:domain].match(/^http/)
-  options[:domain] = 'https://' + options[:domain]
+sitemap = SitemapParser.new(options[:domain] + '/' + options[:sitemap], {recurse: true})
+if sitemap.to_a.empty?
+  puts "No entries in sitemap found"
 end
 
-p options
+pages = sitemap.to_a.collect do |url|
+  html = Nokogiri::HTML(open(url))
+  html.at('meta[name="description"]')
+  {
+    url: url,
+    title: html.at('title')&.inner_text,
+    description: html.at('meta[name="description"]')&.[]('content')&.strip,
+    h1: html.css('h1')&.first&.inner_text&.to_s&.strip
+  }
+end
 
-sitemap = SitemapParser.new(options[:domain] + '/' + options[:sitemap], {recurse: true})
-p sitemap.urls
-
+Axlsx::Package.new do |p|
+  p.workbook.add_worksheet(:name => "Sitemap urls") do |sheet|
+    sheet.add_row %w(url title description h1)
+    pages.each do |page|
+      sheet.add_row [ page[:url], page[:title], page[:description], page[:h1] ]
+    end
+  end
+  p.serialize(options[:excel_file])
+end
